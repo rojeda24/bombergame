0a24f91876cacf1cd2e157fd78dc9b09ab779255
Raul Ojeda <raul.ojeda.luna@gmail.com>
2024-01-14 23:07:41 -0700

    Reduced from 100% to 50% the chance to get a power-up.

--- Assets/Scripts/PowerUps/PowerUpSpawner.cs
+++ Assets/Scripts/PowerUps/PowerUpSpawner.cs
@@ -30,22 +30,23 @@ public class PowerUpSpawner : MonoBehaviour
         while (_spawnQueue.Count > 0)
         {
             var position = _spawnQueue.Dequeue();
-            int random = Random.Range(0, 10);
-            // 30% chance of spawning a more explosion power up
-            if (random < 3)
+            int random = Random.Range(0, 100);
+            // 15% chance of spawning a more explosion power up
+            if (random < 15)
             {
                 Instantiate(_moreExplosionPowerUpPrefab, position, Quaternion.identity);
             }
-            // 40% chance of spawning a +1 bomb power up
-            else if (random < 7)
+            // 20% chance of spawning a +1 bomb power up
+            else if (random < 35)
             {
                 Instantiate(_extraBombPowerUpPrefab, position, Quaternion.identity);
             }
-            // 30% chance of spawning a +1 speed power up
-            else
+            // 15% chance of spawning a +1 speed power up
+            else if (random < 50)
             {
                 Instantiate(_fasterPowerUpPrefab, position, Quaternion.identity);
             }
+            // 50% chance of spawning nothing
         }
     }
 
